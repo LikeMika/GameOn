35dce7b6498c91340330e143c9f8b83cfbbb0920
Michael Bricout <bricout.michael@gmail.com>
2025-01-06 23:25:05 +0100

    Finalisation des validations et messages d'erreurs pour tous les champs

--- modal.js
+++ modal.js
@@ -16,6 +16,9 @@ const emailFields = document.getElementById("email");
 const firstName = document.getElementById("first");
 const lastName = document.getElementById("last");
 const quantity = document.getElementById("quantity");
+const naissance = document.getElementById("birthdate");
+const tournois = document.getElementsByName("location");
+//const conditions = document.getElementById("checkbox1");
 
 const form = document.getElementById("form");
 const error = document.querySelector(".formData");
@@ -62,12 +65,66 @@ function verifierQantity(qant) {
   }
 }
 
+function verifyRadioSelection(groupName) {
+  const radioButtons = document.getElementsByName(groupName);
+  let isChecked = false;
+  console.log(isChecked);
+
+  // Check if at least one radio button is selected
+  for (const radioButton of radioButtons) {
+      if (radioButton.checked) {
+          isChecked = true;
+          break;
+      }
+  }
+
+  // If no radio button is checked, add red border
+  if (!isChecked) {
+      for (const radioButton of radioButtons) {
+        radioButton.parentElement.setAttribute('data-error-visible', 'true');
+      }
+        
+  } else {
+      // Remove red border if a radio button is selected
+      for (const radioButton of radioButtons) {
+        radioButton.parentElement.setAttribute('data-error-visible', 'false');
+      }
+  }
+
+}
+
+function verifySelect(groupName2) {
+  const selectButton = document.getElementById(groupName2);
+  let isChecked2 = false;
+
+
+  // Check if at least one radio button is selected
+      if (selectButton.checked) {
+          isChecked2 = true;
+      }
+
+  // If no radio button is checked, add red border
+  if (!isChecked2) {
+
+    selectButton.parentElement.setAttribute('data-error-visible', 'true');
+        
+  } else {
+      // Remove red border if a radio button is selected
+      selectButton.parentElement.setAttribute('data-error-visible', 'false');
+  }
+  console.log(isChecked2);
+}
+
+
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   verifierChamp(firstName)
   verifierChamp(lastName)
   verifierChamp(emailFields)
   verifierQantity(quantity)
+  verifierChamp(naissance)
+  verifyRadioSelection("location");
+  verifySelect("checkbox1");
 }
 )
 
